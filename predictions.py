6b04b2d23de1295be555293f022bed60b5ea40b8
ishaaty <ishatyagi888@gmail.com>
2023-02-08 07:39:18 -0500

    reduced code

--- predictions.py
+++ predictions.py
@@ -12,19 +12,16 @@ Xdrop = X.drop('Years',1)
 ydrop = y.drop('Years',1)
 
 # reads data used for predictions
-PredictX = pd.read_csv('PredictXv1.csv')
+PredictX = pd.read_csv('PredictXv2.csv')
 PredictXdrop = PredictX.drop('Years',1)
 
 # algorithm
-# Xdrop= Xdrop.reshape(Xdrop.shape[1:])
-
 rf_model_on_full_data = RandomForestRegressor(random_state=1)
 rf_model_on_full_data.fit(Xdrop, ydrop)
 
 # ML model is used to make predictions
 test_preds = rf_model_on_full_data.predict(PredictXdrop)
 
-# output = pd.DataFrame({'Dates': PredictX.Dates, 'Winner': test_preds[0][0], 'IND_Runs' : test_preds[1][1], 'NZ_Runs' : test_preds[2][2], 'IND_Wickets' : test_preds[3][3], 'NZ_Wickets' : test_preds[4][4]})
 output = pd.DataFrame({'Years': PredictX.Years, 'Color' : test_preds})
 
 print(output)
